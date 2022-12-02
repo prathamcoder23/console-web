3b35337a9e35950970c7cad2d021384747a3a155
prathamcoder23 <106394842+prathamcoder23@users.noreply.github.com>
2022-12-02 20:22:53 +0530

    Add files via upload

---
+++ mss.js
@@ -0,0 +1,14 @@
+var  first_name ="akash";
+var last_name ="singh";
+var full_name =first_name.concat(last_name);
+console.log(full_name)
+var a =10;
+var b= 40;
+var add = (a+b);
+console.log(add);
+var sub = (a-b);
+console.log(sub);
+var div = (a/b);
+console.log(div);
+var mulpty = (a*b);
+console.log(mulpty);
